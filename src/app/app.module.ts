118db078b84a648e2c979c888d727f26cd907dae
Adebayo Ajayi <54339202+Braggedtooth@users.noreply.github.com>
2021-08-11 16:28:49 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    RecipesComponent,
+    RecipeDetailsComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
